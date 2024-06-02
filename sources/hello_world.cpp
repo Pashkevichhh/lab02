b346ee3c5c873af7077eda78cea57c38d2618eb9
Pashkevichhh <storchak2005@yandex.ru>
2024-06-02 14:21:13 -0400

    Added comment to prove merge can still be done

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
      std::string name;
-     std::cin >> name;
+     std::cin >> name; // Comment to prove merge can still be done
      std::cout << "Hello world from " << name << std::endl;
      return 0;
 }
