7d6ed14a8de3350e972d21e335d2ca125650c7b1
Pashkevichhh <145035874+Pashkevichhh@users.noreply.github.com>
2024-06-02 21:38:43 +0300

    Update hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-     std::string name;
+     std::string name; // Creating conflict
      std::cin >> name; // Comment to prove merge can still be done
      std::cout << "Hello world from " << name << std::endl;
      return 0;
